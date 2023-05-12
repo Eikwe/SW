f155d66cf87666a6015e458892baadd810d429dc
Projekt <125804811+Eikwe@users.noreply.github.com>
2023-05-12 16:29:08 +0200

    Add files via upload

---
+++ navigation.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+      <li><a href="index.html"> Start </a></li>
+      <li><a href="projekte.html"> Projekte </a>
+        <ul> <li><a href="Solarprojekt.html"> Solarprojekt </a></li> 
+        <li><a href="kunstprojekt.html"> Kunstprojekt </a></li>  </ul></li> 
+      <li><a href="aktuelles.html"> Aktuelles </a>
+        <ul> <li><a href="kabelsammlung.html"> Kabelsammlung </a></li>
+        <li><a href="kunstausstellung.html"> Kunstausstellung </a></li>
+        <li><a href="kompakt.html"> Infos kompakt</a></li></ul> </li>
+      <li><a href="Partnerschaft.html">Partnerschaft </a></li> 
+      <li><a href="englisch.html"> <img class="englisch" src="englisch.png"></a></li>
+    </ul>
+  </nav>

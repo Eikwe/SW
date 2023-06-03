b844bd27932c8926d3690150b3225711f1fb64c7
Projekt <125804811+Eikwe@users.noreply.github.com>
2023-06-03 14:48:17 +0200

    Add files via upload

--- navifooter.js
+++ navifooter.js
@@ -1,6 +1,6 @@
 function loadNavigation() {
   // Navigations-HTML-Code
-  var navigationHTML = `
+  const navigationHTML = `
     <div class="titelcontainer"> 
       <h3> Eikwe-Schweinfurt</h3>
       <nav>
@@ -19,7 +19,7 @@ function loadNavigation() {
               <li><a href="kompakt.html"> Infos kompakt</a></li>
             </ul>
           </li>
-          <li><a href="Partnerschaft.html"> Über uns </a></li> 
+          <li><a href="Partnerschaft.html"> Partnerschaft </a></li> 
           <li><a href="englisch.html"> <img class="englisch" src="englisch.png"></a></li>
         </ul>
       </nav>
@@ -27,7 +27,7 @@ function loadNavigation() {
   `;
 
   // Element mit der ID "navigation" in der index.html suchen
-  var navigationContainer = document.getElementById("navigation");
+  const navigationContainer = document.getElementById("navigation");
 
   // Navigations-HTML-Code in das Element einfügen
   navigationContainer.innerHTML = navigationHTML;
@@ -37,3 +37,38 @@ function loadNavigation() {
 document.addEventListener("DOMContentLoaded", loadNavigation);
 
 
+function loadENavigation() {
+  // Navigations-HTML-Code
+  const EnavigationHTML = `
+  
+  <div class="titelcontainer"  > 
+  <h3> Eikwe-Schweinfurt</h3> 
+  <nav>
+     <ul>
+     <li><a href="englisch.html"> start </a></li>
+     <li><a href="eprojects.html"> projects </a>
+             <ul> <li><a href="eSolarprojekt.html"> solar project </a></li> 
+               <li><a href="eartproject.html"> art project </a></li>  
+             </ul>
+           </li>
+           <li><a href="eaktuell.html"> currently </a>
+             <ul> <li><a href="ekabelsammlung.html"> cable collection </a></li>
+                  <li><a href="eexhibition.html"> exhibition </a></li>
+           </ul></li>
+         <li><a href="ePartnerschaft.html"> partnership </a></li> 
+          <li><a href="index.html"> <img class="englisch" src="deutsch.png" ></a></li>
+         
+     </ul>
+
+  </nav></div>
+  `;
+
+  // Element mit der ID "navigation" in der index.html suchen
+  const EnavigationContainer = document.getElementById("Enavigation");
+
+  // Navigations-HTML-Code in das Element einfügen
+  EnavigationContainer.innerHTML = EnavigationHTML;
+}
+
+// Funktion aufrufen, um die Navigationsleiste zu laden, wenn das DOM geladen ist
+document.addEventListener("DOMContentLoaded", loadENavigation);

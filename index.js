d63682e0bd34a43fe73b7ab2bd6b4f42a902ab00
Projekt <125804811+Eikwe@users.noreply.github.com>
2024-01-09 18:38:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,84 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const prevSlide = document.getElementById("prev-slide");
-  const nextSlide = document.getElementById("next-slide");
-  const figure = document.querySelector("#slider figure");
+  const slider = document.getElementById("slider");
+  const figure = slider.querySelector("figure");
   const images = figure.querySelectorAll("img");
-  const radioButtons = document.querySelectorAll("#slider-controls input[type='radio']");
   let currentIndex = 0;
-  let isAutoplay = true;
-  let autoplayInterval;
 
-  const startAutoplay = function() {
-    autoplayInterval = setInterval(function() {
-      if (isAutoplay) {
-        currentIndex = (currentIndex + 1) % images.length;
-        goToSlide(currentIndex);
-      }
-    }, 3000);
-  };
-
-  const stopAutoplay = function() {
-    clearInterval(autoplayInterval);
-  };
-
-  const goToSlide = function(index) {
-    currentIndex = index;
+  slider.addEventListener("click", (event) => {
+    const direction = event.target.id.includes("prev") ? -1 : 1;
+    currentIndex = (currentIndex + direction + images.length) % images.length;
     figure.style.left = `-${currentIndex * 100}%`;
-    updateRadioButton();
-    stopAutoplay();
-    isAutoplay = false;
-  };
-
-  const goToPrevSlide = function() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    goToSlide(currentIndex);
-  };
-
-  const goToNextSlide = function() {
-    currentIndex = (currentIndex + 1) % images.length;
-    goToSlide(currentIndex);
-  };
-
-  const updateRadioButton = function() {
-    radioButtons[currentIndex].checked = true;
-  };
-
-  prevSlide.addEventListener("click", goToPrevSlide);
-  nextSlide.addEventListener("click", goToNextSlide);
-
-  for (let i = 0; i < radioButtons.length; i++) {
-    radioButtons[i].addEventListener("change", function() {
-      goToSlide(i);
-    });
-  }
-
-  figure.addEventListener("mouseenter", function() {
-    stopAutoplay();
-    isAutoplay = false;
   });
-
-  figure.addEventListener("mouseleave", function() {
-    startAutoplay();
-    isAutoplay = true;
-  });
-
-  startAutoplay();
 });
-
-
-
-//Animation Scroll
-function scrollFunction(targetId) {
-  const link = document.querySelector(`a[href="#${targetId}"]`);
-          
-  link.addEventListener('click', function(event) {
-  event.preventDefault();
-            
-            const element = document.getElementById(targetId);
-            element.scrollIntoView({ behavior: 'smooth' });
-          });
-        }
-        scrollFunktion('gründe')
-        scrollFunction('nabig');
-        scrollFunction('finanzierung');
-        scrollFunction('spenden');

e3f984cd26063f8569887602c5eb496a8c0d208e
Projekt <125804811+Eikwe@users.noreply.github.com>
2024-02-15 18:23:41 +0100

    Delete navi.php

--- navi.php
+++
@@ -1,8 +0,0 @@
-Hallo
-<nav>
-    <ul>
-        <li><a href="index.html">Startseite</a></li>
-        <li><a href="uber.html">Über uns</a></li>
-        <li><a href="kontakt.html">Kontakt</a></li>
-    </ul>
-</nav>
